53a33d0d0b7cb44619a2120dfd35d4f18955286f
Babatunde-odunlami <odunlamibabatunde@outlook.com>
2022-12-08 16:44:14 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Print a line of code using printf function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
