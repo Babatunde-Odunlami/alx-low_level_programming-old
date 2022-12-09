14bb26571e4a2e4c0eaca7331bff43ecfcb23bb0
Babatunde-odunlami <odunlamibabatunde@outlook.com>
2022-12-08 18:24:10 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print a code with date
+ * Return: 1
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
