ed1d937ae7f4e077ebbd9781734594830d85bc6f
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-11 14:18:30 -0800

    task 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Print a code with date
  * Return: 1
 */
 
 int main(void)
-{
-	n = "and that is piece of art is useful";
-
-	fprintf(stderr, "%s \" - Dora Korpar, 2015-10-19\n", n);
+}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
