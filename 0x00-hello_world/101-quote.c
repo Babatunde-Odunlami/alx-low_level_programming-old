33d976f291de39eb7012f38e5b41b7432b1c3007
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-11 14:08:42 -0800

    task 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 
 int main(void)
 {
-	fprintf(stderr, 'and that piece of art is useful" - Dora Korpar, 2015-10-19');
+	n = "and that is piece of art is useful"
+	fprintf(stderr, "%s \" - Dora Korpar, 2015-10-19\n", n);
 	return (1);
 }
