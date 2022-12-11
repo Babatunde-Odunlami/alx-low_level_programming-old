fc236fe40d85961b44eed1405b8affa7818d0b52
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-11 10:45:45 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print number all single numbers from 0 in base 10
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	int j;
+
+	j = 48;
+
+
+	while (j < 58)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+	return (0);
+
+}
