48e5cb09edf5300e9e178427568f7ce66cc538b1
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 15:24:57 -0800

    task 9 and 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: first int to add
+ * @num2: second int to add
+ *
+ * Return: the result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
