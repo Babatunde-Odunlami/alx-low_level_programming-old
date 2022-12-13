9b5df71a177cda94c32d98d391e0af5044bc80f2
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 12:40:10 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing the alphabets 10 times over
+ * Return: 0
+ */
+
+int void print_alphabet_x10(void)
+{
+	int t;
+
+	char letter;
+
+	while (t < 10)
+	{
+		for (letter = 'a', letter <= 'z', letter++;)
+			_putchar(letter);
+	}
+		t++;
+	_putchar('\n');
+	return (0);
+}
