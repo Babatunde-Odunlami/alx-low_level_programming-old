4a08dfbfc3a232a4da3582de54a71123fa0a2a54
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 14:02:13 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - print thesigne of a number
+ * @n: is the number to be checked
+ * Return: 1 if number positive +
+ *  0 if number is zero
+ *  -1 if number is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+		return (-1);
+}
