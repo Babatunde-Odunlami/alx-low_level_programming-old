10139bc34bf34b50a15b1b417928e5b8d289002f
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 13:49:43 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - check if a char is a letter regardless of case
+ * @c: is the char to be checked int
+ * code by Babatunde
+ * Return: 1 if is a letter, otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	else
+		return (0);
+}
