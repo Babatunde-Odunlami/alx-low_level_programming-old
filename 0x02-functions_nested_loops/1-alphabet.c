dd17276cb8a1e9ba79b0e59ddafdeccfd799e5aa
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 11:59:52 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print the alphabets in lowercase and newline
+ * code by Babatunde
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char letter = 97;
+
+	while (letter < 123)
+	{
+		_putchar("%s", letter);
+		letter++;
+	}
+	return (0);
+}
