220ab49b1ee54798116283cd581f1e1643492f3d
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 14:22:44 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the abs value of an int
+ * @n: is the int being checked
+ * code by Babatunde
+ * Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
