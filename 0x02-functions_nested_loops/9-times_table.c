d37d2f928b5dd2a64b983c9ac39c65a405acff98
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-13 17:42:11 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,10 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	if (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0')i;
+		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -19,15 +20,11 @@ void times_table(void)
 			{
 				_putchar(' ');
 			}
-			if (prod > 9)
-			{
-				_putchar((prod / 10) + '0');
-			}
 			else
 			{
-				(prod <= 81 && prod >= 10)
 				_putchar((prod / 10) + '0');
 			}
+				(prod <= 81 && prod >= 10);
 			_putchar((prod % 10) + '0');
 	
 		}
