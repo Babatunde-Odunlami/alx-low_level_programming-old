ea07e58ceac62da2d1bf39c45c09045b5a2f672d
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-19 15:34:26 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * * print_rev -> printing a string in reverse
- * * @s: the string to be printed in rev
- * */
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
+ */
 
 void print_rev(char *s)
 {
 	int i, n;
 
 	n = 0;
-	
+
 	while (s[n] != '\0')
 	{
 		n++;
