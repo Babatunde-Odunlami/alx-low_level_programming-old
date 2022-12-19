01a5b5065fc5a23455e9ef70cc429c937193664b
Babatunde.-odunlami <Odunlamibabatunde@outlook.com>
2022-12-19 14:04:31 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include main.h
+/**
+ * reset_to_98 - function to reset the value of n
+ * @n: variable to be checked
+ * code by Babatunde
+*/
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *ptr_n = &n;
+
+	ptr_n = 98;
+
+	_putchar("%d\n", *&n);
+	return (0);
+}
+
